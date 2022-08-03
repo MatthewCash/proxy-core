8465cb60131a848c0c6198368777d1d255d21d9f
Matthew_Cash <matthew@matthew-cash.com>
2022-08-02 18:10:26 -0700

    Move to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+}
+
+repositories {
+    maven(url = "https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc"
+    }
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.velocitypowered:velocity-api:3.0.1")
+    annotationProcessor("com.velocitypowered:velocity-api:3.0.1")
+    implementation("net.kyori:adventure-text-minimessage:4.11.0")
+    implementation("com.electronwill.night-config:toml:3.6.0")
+    implementation("net.luckperms:api:5.4")
+}
+
+group = "com.matthewcash.network"
+version = "1.0.0"
+description = "Proxy Core"
+java.sourceCompatibility = JavaVersion.VERSION_17
